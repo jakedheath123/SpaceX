e2507ef651e239c9397280fe1810f65693c70189
jakedheath123 <jakedheath@gmail.com>
2020-06-02 13:26:45 +0100

    change name of get all posts variable

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import theSportsDb from "../apis/theSportsDb";
-import { GET_ALL_POSTS } from "./types";
+import { GET_ALL_SPORTS } from "./types";
 
 export const getAllSports = () => dispatch =>
   theSportsDb.get("/all_sports.php").then(response => {
     return dispatch({
-      type: GET_ALL_POSTS,
+      type: GET_ALL_SPORTS,
       payload: response.data
     });
   });
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,1 @@
-export const GET_ALL_POSTS = "GET_ALL_POSTS";
+export const GET_ALL_SPORTS = "GET_ALL_SPORTS";
