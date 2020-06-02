956cc88fbec0e4185621fee7a8eb35e36a5d2662
jakedheath123 <jakedheath@gmail.com>
2020-06-02 13:45:04 +0100

    passed state from getAllSportsReducer to mapStateToProps

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getAllSports } from "../actions";
 
 class HomePage extends Component {
+  componentDidMount() {
+    this.props.getAllSports();
+  }
+
   render() {
+    console.log(this.props);
     return <div>Home Page</div>;
   }
 }
 
-export default HomePage;
+const mapStateToProps = state => {
+  return {
+    allSports: state.sports
+  };
+};
+
+export default connect(mapStateToProps, {
+  getAllSports
+})(HomePage);
