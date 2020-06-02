95efa2c2862891328dfbb7dae67fea5c05a31410
jakedheath123 <jakedheath@gmail.com>
2020-06-02 14:09:58 +0100

    create renderSportsList function to render name of sport and picture on home page component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,9 +7,28 @@ class HomePage extends Component {
     this.props.getAllSports();
   }
 
+  renderSportsList = () => {
+    const { allSports } = this.props;
+    if (allSports.length === 0) return <h1>Loading...</h1>;
+    return (
+      <main>
+        {allSports.map(sport => {
+          return (
+            <li>
+              <ul>
+                <h1>{sport.strSport}</h1>
+                <img src={sport.strSportThumb} alt="Sport Logo" />
+              </ul>
+            </li>
+          );
+        })}
+      </main>
+    );
+  };
+
   render() {
-    console.log(this.props);
-    return <div>Home Page</div>;
+    console.log(this.props.allSports);
+    return <div>{this.renderSportsList()}</div>;
   }
 }
 
