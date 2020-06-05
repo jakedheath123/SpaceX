9161f264df8845395394c22a6360224f96b316f7
jakedheath123 <jakedheath@gmail.com>
2020-06-05 16:10:14 +0100

    add image to sign in page component

--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import logo from "../images/logo.png";
+import image1 from "../images/image1.jpg";
 
 class SignInPage extends Component {
   render() {
-    return <img src={logo} alt="Logo" />;
+    return (
+      <>
+        <img src={logo} alt="Logo" className="ui medium image" />
+        <img src={image1} alt="Rocket launch" />
+      </>
+    );
   }
 }
 
