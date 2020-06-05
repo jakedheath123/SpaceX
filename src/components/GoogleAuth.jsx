8d2458285171d19a7aff52413ff9c92c0650d357
jakedheath123 <jakedheath@gmail.com>
2020-06-05 16:25:29 +0100

    create GoogleAuth component

---
+++ src/components/GoogleAuth.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GoogleAuth = () => {
+  return (
+    <button className="ui red google button">
+      <i className="google icon" />
+      Sign In
+    </button>
+  );
+};
+
+export default GoogleAuth;
