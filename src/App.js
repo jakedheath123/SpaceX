ee99a831949685e43e56370474b43c5cb12c0f5e
jakedheath123 <jakedheath@gmail.com>
2020-06-05 15:55:35 +0100

    import SignInPage component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import { Router } from "@reach/router";
+import SignInPage from "./components/SignInPage";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <HomePage path="/" />
+        <SignInPage path="/" />
+        <HomePage path="/home" />
       </Router>
     </div>
   );
