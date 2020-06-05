ff63a34792c4f5fb14e67547f894f21510752884
jakedheath123 <jakedheath@gmail.com>
2020-06-05 15:55:05 +0100

    create SignInPage component

---
+++ src/components/SignInPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import logo from "../images/logo.png";
+
+class SignInPage extends Component {
+  render() {
+    return <img src={logo} alt="Logo" />;
+  }
+}
+
+export default SignInPage;
