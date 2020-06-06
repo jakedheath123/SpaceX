af62ba5ae3ae7a18419c3fa4e19c3c391e1013a1
jakedheath123 <jakedheath@gmail.com>
2020-06-06 15:16:53 +0100

    import SingleLaunchCard component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import { Router } from "@reach/router";
 import SignInPage from "./components/SignInPage";
+import SingleLaunchCard from "./components/SingleLaunchCard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <SignInPage path="/" />
         <HomePage path="/home" />
+        <SingleLaunchCard path="/launches/:flight_number" />
       </Router>
     </div>
   );
