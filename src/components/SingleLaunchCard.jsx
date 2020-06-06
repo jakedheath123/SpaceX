57b873072d6f0a32809a78060a24fa84cb039990
jakedheath123 <jakedheath@gmail.com>
2020-06-06 16:33:52 +0100

    destructure chosen keys from oneFlight

--- src/components/SingleLaunchCard.jsx
+++ src/components/SingleLaunchCard.jsx
@@ -1,23 +1,46 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getSingleLaunch } from "../actions";
+import Loader from "./Loader";
+import ViewToggler from "./ViewToggler";
 
-const SingleLaunchCard = ({ getSingleLaunch, flight_number }) => {
+const SingleLaunchCard = ({ getSingleLaunch, flight_number, oneFlight }) => {
   useEffect(() => {
     getSingleLaunch(flight_number);
   }, [flight_number]);
 
+  if (!oneFlight) return <Loader />;
+
+  const {
+    mission_name,
+    details,
+    launch_date_local,
+    links,
+    rocket,
+    launch_failure_details
+  } = oneFlight;
+  const {
+    mission_patch_small,
+    article_link,
+    video_link,
+    flickr_images
+  } = links;
+  const { rocket_name, rocket_type } = rocket;
+  const { time, reason } = launch_failure_details;
   return (
-    <div>
-      <h1>Singe Launch Card</h1>
-    </div>
+    <main>
+      <h1>{mission_name}</h1>
+      <ViewToggler></ViewToggler>
+    </main>
   );
 };
 
-// const mapStateToProps = state => {
-//   console.log(state);
-// };
+const mapStateToProps = state => {
+  return {
+    oneFlight: state.singleLaunch.singleFlight
+  };
+};
 
-export default connect(null, {
+export default connect(mapStateToProps, {
   getSingleLaunch
 })(SingleLaunchCard);
