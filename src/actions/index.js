96232b600a4f68a35259f095fb5bb8c18949831c
jakedheath123 <jakedheath@gmail.com>
2020-06-06 17:34:21 +0100

    create toggleVisible action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import spaceXDb from "../apis/spaceXDb";
-import { GET_LAUNCHES, SIGN_IN, GET_SINGLE_LAUNCH } from "./types";
+import { GET_LAUNCHES, SIGN_IN, GET_SINGLE_LAUNCH, IS_VISIBLE } from "./types";
 
 export const getLaunches = () => dispatch =>
   spaceXDb.get("/launches?limit=20").then(response => {
@@ -22,5 +22,11 @@ export const signIn = userId => {
   return {
     type: SIGN_IN,
     payload: userId
+  };
+};
+
+export const toggleVisible = () => {
+  return {
+    type: IS_VISIBLE
   };
 };
