a7b5b9a4b0012e766548d136b7f55d33bbd4df7c
jakedheath123 <jakedheath@gmail.com>
2020-06-06 17:32:04 +0100

    change name of viewTogglerReducer key

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import getAllLaunchesReducer from "./getAllLaunchesReducer";
 import googleAuthReducer from "./googleAuthReducer";
 import getSingleLaunchReducer from "./getSingleLaunchReducer";
+import viewTogglerReducer from "./viewTogglerReducer";
 
 export default combineReducers({
   launches: getAllLaunchesReducer,
   auth: googleAuthReducer,
-  singleLaunch: getSingleLaunchReducer
+  singleLaunch: getSingleLaunchReducer,
+  showOrHide: viewTogglerReducer
 });
