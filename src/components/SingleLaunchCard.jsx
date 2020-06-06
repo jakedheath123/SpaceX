196feeb81fca72d3000ca2fc60351d58d2799643
jakedheath123 <jakedheath@gmail.com>
2020-06-06 15:45:02 +0100

    import getSingleLaunch action creator to trigger with useEffect

--- src/components/SingleLaunchCard.jsx
+++ src/components/SingleLaunchCard.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getSingleLaunch } from "../actions";
 
-const SingleLaunchCard = () => {
+const SingleLaunchCard = ({ getSingleLaunch, flight_number }) => {
+  useEffect(() => {
+    getSingleLaunch(flight_number);
+  }, [flight_number]);
+
   return (
     <div>
       <h1>Singe Launch Card</h1>
@@ -8,4 +14,10 @@ const SingleLaunchCard = () => {
   );
 };
 
-export default SingleLaunchCard;
+// const mapStateToProps = state => {
+//   console.log(state);
+// };
+
+export default connect(null, {
+  getSingleLaunch
+})(SingleLaunchCard);
