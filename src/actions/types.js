66ceabf7cbcfc357340df9e433fd9ca24fdfad8a
jakedheath123 <jakedheath@gmail.com>
2020-06-07 13:53:19 +0100

    create GET_COMPANY_INFO variable

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,4 @@ export const SIGN_IN = "SIGN_IN";
 export const GET_SINGLE_LAUNCH = "GET_SINGLE_LAUNCH";
 export const IS_VISIBLE = "IS_VISIBLE";
 export const SET_FALSE = "SET_FALSE";
+export const GET_COMPANY_INFO = "GET_COMPANY_INFO";
