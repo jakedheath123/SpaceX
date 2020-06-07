5388fce33b0dd526b36a01a85a70ff57b80af121
jakedheath123 <jakedheath@gmail.com>
2020-06-07 13:42:34 +0100

    import About component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import "./App.css";
-import HomePage from "./components/HomePage";
 import { Router } from "@reach/router";
+
+import "./App.css";
 import SignInPage from "./components/SignInPage";
 import SingleLaunchCard from "./components/SingleLaunchCard";
+import HomePage from "./components/HomePage";
+import About from "./components/About";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <SignInPage path="/" />
         <HomePage path="/home" />
         <SingleLaunchCard path="/launches/:flight_number" />
+        <About path="/about" />
       </Router>
     </div>
   );
