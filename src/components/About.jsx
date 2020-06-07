d6b26328721e40c93b317c9b97e873d65d98a7f7
jakedheath123 <jakedheath@gmail.com>
2020-06-07 14:13:50 +0100

    destructured chosen keys from spaceXInfo object

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,29 @@ import { connect } from "react-redux";
 
 import { getCompanyInfo } from "../actions";
 
-const About = ({ getCompanyInfo }) => {
+const About = ({ getCompanyInfo, spaceXInfo }) => {
   useEffect(() => {
     getCompanyInfo();
   }, []);
+
+  const {
+    name,
+    founder,
+    founded,
+    employees,
+    vehicles,
+    launch_sites,
+    ceo,
+    cto,
+    coo,
+    cto_propulsion,
+    valuation,
+    summary,
+    headquarters,
+    links
+  } = spaceXInfo;
+  const { address, city, state } = headquarters;
+  const { website, twitter, elon_twitter } = links;
 
   return (
     <div>
@@ -19,7 +38,11 @@ const About = ({ getCompanyInfo }) => {
   );
 };
 
-const mapStateToProps = state => {};
+const mapStateToProps = state => {
+  return {
+    spaceXInfo: state.companyInfo.companyDetails
+  };
+};
 
 export default connect(mapStateToProps, {
   getCompanyInfo
