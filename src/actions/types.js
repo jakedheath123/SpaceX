82bf3d41d98d7023c54ba69bf8a61b515b502fd9
jakedheath123 <jakedheath@gmail.com>
2020-06-12 13:19:07 +0100

    change name of SET_TRUE variable

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,4 +4,4 @@ export const GET_SINGLE_LAUNCH = "GET_SINGLE_LAUNCH";
 export const IS_VISIBLE = "IS_VISIBLE";
 export const SET_FALSE = "SET_FALSE";
 export const GET_COMPANY_INFO = "GET_COMPANY_INFO";
-export const SET_TRUE = "SET_TRUE";
+export const HAS_ERRORED = "HAS_ERRORED";
