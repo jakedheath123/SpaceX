c7340125b770f730785a358ae74ccc1a1eee0906
jakedheath123 <jakedheath@gmail.com>
2020-06-12 14:09:18 +0100

    remove errorBoundaryReducer from combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,13 +5,11 @@ import googleAuthReducer from "./googleAuthReducer";
 import getSingleLaunchReducer from "./getSingleLaunchReducer";
 import viewTogglerReducer from "./viewTogglerReducer";
 import getCompanyInfoReducer from "./getCompanyInfoReducer";
-import errorBoundaryReducer from "./errorBoundaryReducer";
 
 export default combineReducers({
   launches: getAllLaunchesReducer,
   auth: googleAuthReducer,
   singleLaunch: getSingleLaunchReducer,
   showOrHide: viewTogglerReducer,
-  companyInfo: getCompanyInfoReducer,
-  errorBoundary: errorBoundaryReducer
+  companyInfo: getCompanyInfoReducer
 });
