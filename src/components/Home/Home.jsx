43e66fbe75a7a3047822cb7c03736cd714642370
jakedheath123 <jakedheath@gmail.com>
2020-06-22 21:40:08 +0100

    removed home page image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "@reach/router";
 
 import "./Home.css";
-import starLink from "../../images/starLink.jpg";
 
 const HomePage = () => {
   return (
@@ -14,7 +13,7 @@ const HomePage = () => {
         </Link>
       </div>
       <div className="home-box2">
-        <img src={starLink} alt="rocket" />
+        {/* <img src={starLink} alt="rocket" /> */}
       </div>
       <div className="home-box3">
         <h1>Next Launch:</h1>
