1b0b66c8d5c59a248d699a67b1d1d51fd3cd6df2
jakedheath123 <jakedheath@gmail.com>
2020-06-24 15:32:37 +0100

    display launch totals and launch success rate

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "@reach/router";
 import { connect } from "react-redux";
 
 import { getLaunches } from "../../actions";
 import "./Home.css";
 import earth from "../../images/earth.jpg";
+import Loader from "../Loader/Loader";
 
-const HomePage = () => {
+const HomePage = ({ getLaunches, launches }) => {
+  useEffect(() => {
+    getLaunches();
+  }, [getLaunches]);
+
+  if (!launches) return <Loader />;
+
+  const totalLaunches = launches.length;
+  const successfulLaunchTotal = launches.reduce((total, launch) => {
+    if (launch.launch_success) total++;
+    return total;
+  }, 0);
+
   return (
     <main className="home-container">
       <div className="home-box1">
-        <h1>Latest Launch:</h1>
+        <h2>Latest Launch:</h2>
         <Link to="/latest">
           <button>Learn More</button>
         </Link>
@@ -19,13 +32,31 @@ const HomePage = () => {
         <img src={earth} alt="rocket" />
       </div>
       <div className="home-box3">
-        <h1>Next Launch:</h1>
+        <h2>Next Launch:</h2>
         <Link to="/next">
           <button>Learn More</button>
         </Link>
+      </div>
+      <div className="home-box4">
+        <h2>{totalLaunches ? totalLaunches : null}</h2>
+        <h3>Total Launches</h3>
+      </div>
+      <div className="home-box5">
+        <h2>
+          {successfulLaunchTotal
+            ? `${Math.round((successfulLaunchTotal / totalLaunches) * 100)}%`
+            : null}
+        </h2>
+        <h3>Launch Success</h3>
       </div>
     </main>
   );
 };
 
-export default connect(null, { getLaunches })(HomePage);
+const mapStateToProps = state => {
+  return {
+    launches: state.launches
+  };
+};
+
+export default connect(mapStateToProps, { getLaunches })(HomePage);
