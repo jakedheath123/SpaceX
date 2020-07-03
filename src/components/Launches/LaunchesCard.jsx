145eb3a1b3935c4974312c6487392d27f079a084
jakedheath123 <jakedheath@gmail.com>
2020-07-03 19:21:00 +0100

    add styling and image to launches component

--- src/components/Launches/LaunchesCard.jsx
+++ src/components/Launches/LaunchesCard.jsx
@@ -8,17 +8,16 @@ const LaunchesCard = ({ launch, setTogglerFalse }) => {
   const {
     flight_number,
     mission_name,
-    launch_success /*launch_year*/
+    launch_success,
+    links: { mission_patch_small }
   } = launch;
 
   return (
     <li>
-      {/* <div> */}
+      <img src={mission_patch_small} alt="Mission patch" />
       <h1 style={launch_success ? { color: "green" } : { color: "red" }}>
         {mission_name}{" "}
       </h1>
-      {/* <h2>{launch_year}</h2> */}
-      {/* </div> */}
       <Link to={`/launches/${flight_number}`} className="launches-link">
         <button onClick={setTogglerFalse} className="ui secondary button">
           Launch Details
