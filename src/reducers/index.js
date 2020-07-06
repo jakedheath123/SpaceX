bd290dffc3d80feebe9380ff00572fb5f26153e3
jakedheath123 <jakedheath@gmail.com>
2020-07-06 15:23:28 +0100

    delete search by year reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from "redux";
 import getAllLaunchesReducer from "./getAllLaunchesReducer";
 import getSingleLaunchReducer from "./getSingleLaunchReducer";
 import viewTogglerReducer from "./viewTogglerReducer";
-import searchByYearReducer from "./searchByYearReducer";
 
 export default combineReducers({
   launches: getAllLaunchesReducer,
   singleLaunch: getSingleLaunchReducer,
-  showOrHide: viewTogglerReducer,
-  searchByYear: searchByYearReducer
+  showOrHide: viewTogglerReducer
 });
