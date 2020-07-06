fc1dbd19ec89e78746bd5d257905bce5d1fa071b
jakedheath123 <jakedheath@gmail.com>
2020-07-06 13:54:52 +0100

    add search by year reducer to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import getAllLaunchesReducer from "./getAllLaunchesReducer";
 import getSingleLaunchReducer from "./getSingleLaunchReducer";
 import viewTogglerReducer from "./viewTogglerReducer";
+import searchByYearReducer from "./searchByYearReducer";
 
 export default combineReducers({
   launches: getAllLaunchesReducer,
   singleLaunch: getSingleLaunchReducer,
-  showOrHide: viewTogglerReducer
+  showOrHide: viewTogglerReducer,
+  searchByYear: searchByYearReducer
 });
