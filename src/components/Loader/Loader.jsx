bf577d2b3d733befcc24723038ddb6b78ac15e39
jakedheath123 <jakedheath@gmail.com>
2020-07-06 20:16:22 +0100

    add in line styling

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,10 @@ import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="lds-ripple">
+    <div
+      className="lds-ripple"
+      style={{ width: "300px", margin: "auto", display: "block" }}
+    >
       <div></div>
       <div></div>
     </div>
