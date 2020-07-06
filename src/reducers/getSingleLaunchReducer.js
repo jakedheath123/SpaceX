683cbda94f80c3d5df2ba97d92a75b8d5a1b506b
jakedheath123 <jakedheath@gmail.com>
2020-07-06 20:14:34 +0100

    add isLoading key

--- src/reducers/getSingleLaunchReducer.js
+++ src/reducers/getSingleLaunchReducer.js
@@ -1,12 +1,12 @@
 const INITIAL_STATE = {
-  singleFlight: null,
+  singleLaunch: null,
   isLoading: true
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "GET_SINGLE_LAUNCH":
-      return { ...state, singleFlight: action.payload, isLoading: false };
+      return { ...state, singleLaunch: action.payload, isLoading: false };
     default:
       return state;
   }
