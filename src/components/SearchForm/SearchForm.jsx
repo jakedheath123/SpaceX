418504322632b40b5368b97708d9804a6c92a436
jakedheath123 <jakedheath@gmail.com>
2020-07-06 20:16:09 +0100

    refactor form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,82 +1,81 @@
 import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
 
-import { getLaunches } from "../../actions";
-
-const SearchForm = ({ searchByLaunchYear }) => {
+const SearchForm = ({ getQuery }) => {
   const [userSelection, setUserSelection] = useState("");
 
   useEffect(() => {
-    searchByLaunchYear(userSelection);
-  }, [searchByLaunchYear, userSelection]);
+    getQuery(userSelection);
+  }, [getQuery, userSelection]);
 
-  const handleChange = event => {
-    const { value } = event.target;
+  const onChange = value => {
     setUserSelection(value);
   };
 
   return (
-    <form>
-      <label>
-        Search By Year:
-        <select onChange={handleChange} value={userSelection}>
-          <option name="allYears" value="">
-            All Years(Default)
-          </option>
-          <option name="2021" value="2021">
-            2021
-          </option>
-          <option name="2020" value="2020">
-            2020
-          </option>
-          <option name="2019" value="2019">
-            2019
-          </option>
-          <option name="2018" value="2018">
-            2018
-          </option>
-          <option name="2017" value="2017">
-            2017
-          </option>
-          <option name="2016" value="2016">
-            2016
-          </option>
-          <option name="2015" value="2015">
-            2015
-          </option>
-          <option name="2014" value="2014">
-            2014
-          </option>
-          <option name="2013" value="2013">
-            2013
-          </option>
-          <option name="2012" value="2012">
-            2012
-          </option>
-          <option name="2011" value="2011">
-            2011
-          </option>
-          <option name="2010" value="2010">
-            2010
-          </option>
-          <option name="2009" value="2009">
-            2009
-          </option>
-          <option name="2008" value="2008">
-            2008
-          </option>
-          <option name="2007" value="2007">
-            2007
-          </option>
-          <option name="2006" value="2006">
-            2006
-          </option>
-        </select>
-      </label>
-    </form>
+    <section>
+      <form>
+        <label>
+          Search By Year:
+          <select
+            onChange={event => onChange(event.target.value)}
+            value={userSelection}
+          >
+            <option name="allYears" value="">
+              All Years(Default)
+            </option>
+            <option name="2021" value="2021">
+              2021
+            </option>
+            <option name="2020" value="2020">
+              2020
+            </option>
+            <option name="2019" value="2019">
+              2019
+            </option>
+            <option name="2018" value="2018">
+              2018
+            </option>
+            <option name="2017" value="2017">
+              2017
+            </option>
+            <option name="2016" value="2016">
+              2016
+            </option>
+            <option name="2015" value="2015">
+              2015
+            </option>
+            <option name="2014" value="2014">
+              2014
+            </option>
+            <option name="2013" value="2013">
+              2013
+            </option>
+            <option name="2012" value="2012">
+              2012
+            </option>
+            <option name="2011" value="2011">
+              2011
+            </option>
+            <option name="2010" value="2010">
+              2010
+            </option>
+            <option name="2009" value="2009">
+              2009
+            </option>
+            <option name="2008" value="2008">
+              2008
+            </option>
+            <option name="2007" value="2007">
+              2007
+            </option>
+            <option name="2006" value="2006">
+              2006
+            </option>
+          </select>
+        </label>
+      </form>
+    </section>
   );
 };
 
-export default connect(null, {
-  getLaunches
-})(SearchForm);
+export default SearchForm;
