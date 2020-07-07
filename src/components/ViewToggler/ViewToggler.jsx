6c86f4d952f7b42e41e761fc1bd7e3f31deb8fcb
jakedheath123 <jakedheath@gmail.com>
2020-07-07 15:25:38 +0100

    add style changes to content

--- src/components/ViewToggler/ViewToggler.jsx
+++ src/components/ViewToggler/ViewToggler.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { toggleVisible } from "../../actions";
+import "./ViewToggler.css";
 
 const ViewToggler = ({ isVisible, children, toggleVisible }) => {
   return (
-    <section>
+    <div className="view-toggler-container">
       <button onClick={toggleVisible} className="ui button">
         {isVisible ? "Hide Flickr Images" : "Display Flickr Images"}
       </button>
+      <br></br>
       {isVisible && children}
-    </section>
+    </div>
   );
 };
 
