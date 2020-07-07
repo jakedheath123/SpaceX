ffcf52496ad6bb50771e8ee15914e9940d43cc0f
jakedheath123 <jakedheath@gmail.com>
2020-07-07 23:35:09 +0100

    rename case

--- src/reducers/allLaunchesReducer.js
+++ src/reducers/allLaunchesReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "GET_LAUNCHES":
+    case "GET_ALL_LAUNCHES":
       return { ...state, data: action.payload, isLoading: false };
     default:
       return state;
