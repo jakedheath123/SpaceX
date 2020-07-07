55fa3763a6508c580176757435fdf9aeb2800ed3
jakedheath123 <jakedheath@gmail.com>
2020-07-07 23:35:25 +0100

    rename variable

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,4 @@
-export const GET_LAUNCHES = "GET_LAUNCHES";
+export const GET_ALL_LAUNCHES = "GET_ALL_LAUNCHES";
 export const GET_SINGLE_LAUNCH = "GET_SINGLE_LAUNCH";
 export const IS_VISIBLE = "IS_VISIBLE";
 export const SET_TOGGLER_FALSE = "SET_TOGGLER_FALSE";
