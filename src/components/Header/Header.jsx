5face13d403c9ee5c766edb03618c79f1d2c29ed
jakedheath123 <jakedheath@gmail.com>
2020-07-08 12:56:15 +0100

    refactor header component styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,8 @@ import logo from "../../images/logo.jpg";
 
 const Header = () => {
   return (
-    <header className="header-container">
-      <div className="header-logo">
-        <img src={logo} alt="spaceX logo" />
-      </div>
+    <header className="header-logo">
+      <img src={logo} alt="spaceX logo" />
     </header>
   );
 };
