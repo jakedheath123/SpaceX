b45c5a4701b62fe0c095fa4188d783791f102cf4
jakedheath123 <jakedheath@gmail.com>
2020-07-15 12:57:37 +0100

    import single launch card component

--- src/components/SingleLaunch/SingleLaunch.jsx
+++ src/components/SingleLaunch/SingleLaunch.jsx
@@ -1,66 +1,24 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { Link } from "@reach/router";
 
 import "./SingleLaunch.css";
-import { getSingleLaunch, setTogglerFalse } from "../../actions";
 import Loader from "../Loader/Loader";
-import ViewToggler from "../ViewToggler/ViewToggler";
-import SingleLaunchImages from "./SingleLaunchImages";
+import SingleLaunchCard from "./SingleLaunchCard";
 
 const SingleLaunch = ({
   getSingleLaunch,
   flight_number,
-  data,
-  isLoading,
+  singleLaunch: { data, isLoading },
   setTogglerFalse
 }) => {
   useEffect(() => {
     getSingleLaunch(flight_number);
   }, [getSingleLaunch, flight_number]);
 
-  if (isLoading) return <Loader />;
-
-  const {
-    mission_name,
-    details,
-    launch_year,
-    links: { mission_patch_small, flickr_images }
-  } = data;
-
-  return (
-    <main className="single-launch-container">
-      <section className="single-launch-info">
-        <Link to="/" className="link-button">
-          <button onClick={setTogglerFalse} className="ui button">
-            Back
-          </button>
-        </Link>
-        <h1>{mission_name}</h1>
-        <h2>{launch_year}</h2>
-        {mission_patch_small ? (
-          <img src={mission_patch_small} alt="Mission Patch" />
-        ) : null}
-        <p>{details ? `Mission Details: ${details}` : null}</p>
-      </section>
-      <section className="single-launch-toggler">
-        <ViewToggler>
-          <SingleLaunchImages images={flickr_images} />
-        </ViewToggler>
-      </section>
-    </main>
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <SingleLaunchCard launch={data} setTogglerFalse={setTogglerFalse} />
   );
 };
 
-const mapStateToProps = state => {
-  const { data, isLoading } = state.singleLaunch;
-  return {
-    data,
-    isLoading
-  };
-};
-
-export default connect(mapStateToProps, {
-  getSingleLaunch,
-  setTogglerFalse
-})(SingleLaunch);
+export default SingleLaunch;
