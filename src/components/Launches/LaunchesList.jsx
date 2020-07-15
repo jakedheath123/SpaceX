2d8f9322d4ea19ecdd84e02a69981e6357197c2b
jakedheath123 <jakedheath@gmail.com>
2020-07-15 15:50:15 +0100

    import LaunchesCard component

--- src/components/Launches/LaunchesList.jsx
+++ src/components/Launches/LaunchesList.jsx
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import "./LaunchesList.css";
 import LaunchesCard from "./LaunchesCard";
-import Loader from "../Loader/Loader";
-import SearchForm from "../SearchForm/SearchForm";
 
-const LaunchesList = ({ getAllLaunches, allLaunches: { data, isLoading } }) => {
-  const [launchYear, setLaunchYear] = useState("");
-
-  useEffect(() => {
-    getAllLaunches(launchYear);
-  }, [getAllLaunches, launchYear]);
-
-  return (
-    <main className="launches-container">
-      <SearchForm
-        getQuery={userSelect => setLaunchYear(userSelect)}
-        className="launches-search-form"
-      />
-      <aside className="launches-keys">
-        <h1>Launches</h1>
-        <p>
-          <i className="square icon" style={{ color: "green" }} /> = Success
-        </p>
-        <p>
-          <i className="square icon" style={{ color: "red" }} /> = Fail
-        </p>
-      </aside>
-      <section className="launches-card">
-        {isLoading ? <Loader /> : <LaunchesCard launches={data} />}
-      </section>
-    </main>
+const LaunchesList = ({ launches }) => {
+  return !launches.length ? (
+    <p>No Launches</p>
+  ) : (
+    <ul>
+      {launches.map(launch => {
+        const { flight_number } = launch;
+        return <LaunchesCard key={flight_number} launch={launch} />;
+      })}
+    </ul>
   );
 };
 
