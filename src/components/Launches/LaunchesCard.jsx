c62a293e33aabf00565c52f5f9b741f2b03998a5
jakedheath123 <jakedheath@gmail.com>
2020-07-15 12:59:03 +0100

    create launches card component

--- src/components/Launches/LaunchesCard.jsx
+++ src/components/Launches/LaunchesCard.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-const LaunchesCard = ({
-  launch: {
-    flight_number,
-    mission_name,
-    launch_success,
-    links: { mission_patch_small }
-  }
-}) => {
-  return (
-    <li>
-      {mission_patch_small ? (
-        <img src={mission_patch_small} alt="Mission patch" />
-      ) : null}
+const LaunchesCard = ({ launches }) => {
+  return !launches.length ? (
+    <p>No Launches</p>
+  ) : (
+    <ul>
+      {launches.map(launch => {
+        const {
+          flight_number,
+          mission_name,
+          launch_success,
+          links: { mission_patch_small }
+        } = launch;
+        return (
+          <li key={flight_number}>
+            {mission_patch_small ? (
+              <img src={mission_patch_small} alt="Mission patch" />
+            ) : null}
 
-      <h1 style={launch_success ? { color: "green" } : { color: "red" }}>
-        {mission_name}{" "}
-      </h1>
-      <Link to={`/launches/${flight_number}`} className="launches-link">
-        <button className="ui secondary button">Launch Details</button>
-      </Link>
-    </li>
+            <h1 style={launch_success ? { color: "green" } : { color: "red" }}>
+              {mission_name}{" "}
+            </h1>
+            <Link to={`/launches/${flight_number}`} className="launches-link">
+              <button className="ui secondary button">Launch Details</button>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
