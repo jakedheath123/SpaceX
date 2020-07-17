c97162bdbbbd563857c3cb60f1b9136ca997ae05
jakedheath123 <jakedheath@gmail.com>
2020-07-17 16:50:14 +0100

    create launches card folder

---
+++ src/components/Launches/LaunchesCard/LaunchesCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+const LaunchesCard = ({ launch }) => {
+  const {
+    flight_number,
+    mission_name,
+    launch_success,
+    links: { mission_patch_small }
+  } = launch;
+  return (
+    <li key={flight_number}>
+      {mission_patch_small ? (
+        <img src={mission_patch_small} alt="Mission patch" />
+      ) : null}
+      <h1 style={launch_success ? { color: "green" } : { color: "red" }}>
+        {mission_name}{" "}
+      </h1>
+      <Link to={`/launches/${flight_number}`} className="launches-link">
+        <button className="ui secondary button">Launch Details</button>
+      </Link>
+    </li>
+  );
+};
+
+export default LaunchesCard;
